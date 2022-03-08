871c5b8deebea14a78e132a0f2fbe022c6b74393
SYSTHERM\mslany <mslany@systherm-info.pl>
2022-03-08 14:52:35 +0100

    obsługa katalogów w git

--- HelloWorldApp/HelloWorldApp/Program.cs
+++ HelloWorldApp/HelloWorldApp/Program.cs
@@ -26,7 +26,12 @@ namespace HelloWorldApp
     {
       Application.SetHighDpiMode(HighDpiMode.SystemAware);
 
+<<<<<<< Updated upstream
 
+=======
+      // poprawka w brach_1
+      // poprawka w brach_2
+>>>>>>> Stashed changes
 
       Application.EnableVisualStyles();
       Application.SetCompatibleTextRenderingDefault(false);
